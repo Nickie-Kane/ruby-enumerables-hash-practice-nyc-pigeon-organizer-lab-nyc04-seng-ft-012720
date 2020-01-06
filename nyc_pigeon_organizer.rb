3e96abf6475b6515389f1742fb598e62dec2cb8e
nickie kane <kameo712@gmail.com>
2020-01-06 02:09:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,25 +14,3 @@ def nyc_pigeon_organizer(data)
   # end
 end  
 
-
-
-  # return pigeon_list
-  # data[:color][i]
-  # data[:color][:purple][i]
-  # data[:color][:white][i]
-  # data[:color][:brown][i]
-  # data[:gender][i]
-  # data[:gender][:male][i]
-  # data[:gender][:female][i]
-  # data[:lives]
-  # data[:lives]["Subway"]
-  # data[:lives]["Central Park"]
-  # data[:lives]["Library"]
-  # data[:lives]["City Hall"]
-# entry = pigeon[i]=>"0"
-# nyc_pigeon_organizer(entry) do |v|
-#   puts v
-# end
-   
-  # binding.pry
-end
