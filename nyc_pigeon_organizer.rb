70af04a80780f1ededa8b5d7f085d796371dde5d
nickie kane <kameo712@gmail.com>
2020-01-06 02:01:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+   pigeon_list = {}
+  # binding.pry
+   pigeon = data[:gender][:male] + data[:gender][:female]
+   i = 0 
+   while i < pigeon.length do
+     puts entry
+     yield(entry)
+      pigeon_list = {entry}
+   i += 1 
+   end
+   binding.pry
+end   
+  # return pigeon_list
+  # data[:color][i]
+  # data[:color][:purple][i]
+  # data[:color][:white][i]
+  # data[:color][:brown][i]
+  # data[:gender][i]
+  # data[:gender][:male][i]
+  # data[:gender][:female][i]
+  # data[:lives]
+  # data[:lives]["Subway"]
+  # data[:lives]["Central Park"]
+  # data[:lives]["Library"]
+  # data[:lives]["City Hall"]
+entry = pigeon[i]=>"0"
+nyc_pigeon_organizer(entry) do |v|
+   puts v
+   
+  # binding.pry
 end
