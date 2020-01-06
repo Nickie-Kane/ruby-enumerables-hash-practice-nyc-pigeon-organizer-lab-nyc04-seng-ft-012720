3c7b3840b22ee4d8ffec5e0861675e74dd372305
nickie kane <kameo712@gmail.com>
2020-01-06 02:09:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,14 +4,14 @@ def nyc_pigeon_organizer(data)
     binding.pry
     
    
-   data.each do |key, value|
-     value.each do |des, arr|
-       arr.each do |name|
-         if pigeon.has_key?(name)
-            if pigeon[name].has_key?(key)
-       end 
-     end 
-   end
+  # data.each do |key, value|
+  #   value.each do |des, arr|
+  #     arr.each do |name|
+  #       if pigeon.has_key?(name)
+  #           if pigeon[name].has_key?(key)
+  #     end 
+  #   end 
+  # end
 end  
 
 
